ff075f80a2d2c3b040a25c6488dba21c23dc2546
pedrob <ppedrohenrique1@gmail.com>
2024-03-11 20:31:52 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    default:
+        console.log(" Invalid Month");
+        break;
+}
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+var i = 0;
+
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+function dizHello() {
+    console.log("Hello");
+}
+
+dizHello();
+
+function dizBye(text, text1) {
+    console.log(text, text1);   
+}
+
+dizBye("Bye", "World");
+
+
+function outpost(text) {
+    console.log(text);   
+}
+
+outpost("Bye", "World");
